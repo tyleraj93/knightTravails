2a2e864f81f7310c08f331e7c41ca4aadaf2d0d4
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2024-07-14 20:11:07 -0400

    Initial build with webpack

---
+++ index.bundle.js
@@ -0,0 +1,19 @@
+(self["webpackChunkknighttravails"] = self["webpackChunkknighttravails"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("Hello, world");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9rbmlnaHR0cmF2YWlscy8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIkhlbGxvLCB3b3JsZFwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
